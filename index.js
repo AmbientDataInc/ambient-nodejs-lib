66573b10fab90ef81b56697fb5eb129d44020c74
Takehiko Shimojima <shimojima@ambidata.io>
2021-05-02 16:17:31 +0900

    debug

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-var Qs = require('qs');
-var axios = require('axios');
-var ambient = require('./lib/ambient-lib');
-module.exports = ambient.Ambient;
+module.exports = require('./lib/ambient-lib');
